71f5e289085f5e67cdd97ad809a4aba2ba046b43
unknown <jmbrown864@gmail.com>
2024-03-05 01:46:19 -0800

    site skeleton

--- src/components/PlaylistCreator.js
+++ src/components/PlaylistCreator.js
@@ -3,15 +3,37 @@ import React from 'react'
 export default function PlaylistCreator() {
     return (
         <div>
-            <div>
-                Search Bar
+
+            <div className="search" class="flex justify-center p-4 mt-2"> {/* search box */}
+                <form action="/">
+                    <label>
+                        <span class="block text-sm font-medium text-slate-700">Search for...</span>
+                        <input class="text-center text-sm placeholder-slate-400 rounded-md border border-slate-300 shadow-sm p-2 sm:pl-24 sm:pr-24 md:pl-48 md:pr-48 focus:outline-none focus:border-sky-500" placeholder="Search for..."></input>
+                    </label>
+                </form>
+            </div> { /* end search box */}
+
+            <div class="w-full p-2 grid md:grid-cols-2 sm:gap-2 md:gap-10">
+                <div class="md:col-span-1"> { /* search results */}
+                    <h2 class="font-bold m-6 pb-2 border-b border-gray-200">Search Results</h2>
+                    <div class="m-6"> { /* search results content */}
+                        <div>
+                            Song Info
+                        </div>
+                        <div>
+                            Actions
+                        </div>
+                    </div>
+                </div> { /* end search results */}
+
+                <div class="md:col-span-1"> { /* add to playlist */}
+                    <h2 class="font-bold m-6 pb-2 border-b border-gray-200">Create Playlist</h2>
+                    <div> { /* search results content */}
+
+                    </div>
+                </div> { /* end add to playlist */}
             </div>
-            <div>
-                Search Results
-            </div>
-            <div>
-                Playlist
-            </div>
+
         </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}"
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/components/PlaylistCreator.js"
   ],
   theme: {
     extend: {},
   },
   plugins: [
     require('tailwindcss'),
-    require('autoprefixer'),
+    require('autoprefixer')
   ],
 }
 
