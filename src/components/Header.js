f2421060cc8c12a2de62d799697e75de5e7f424a
unknown <jmbrown864@gmail.com>
2024-03-05 00:56:28 -0800

    initial commit - create-react-app, tailwind config, cleanup, and site skeleton

---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div class="text-gray-700 text-2xl font-bold bg-slate-200 flex justify-center p-3">Playlist Creator</div>
+  )
+}
---
+++ src/components/PlaylistCreator.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function PlaylistCreator() {
+    return (
+        <div>
+            <div>
+                Search Bar
+            </div>
+            <div>
+                Search Results
+            </div>
+            <div>
+                Playlist
+            </div>
+        </div>
+    )
+}
