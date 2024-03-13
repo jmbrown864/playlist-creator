6a0fa772f84f48cc9a8fa713ffae9decef16ac05
unknown <jmbrown864@gmail.com>
2024-03-12 17:41:28 -0700

    get access token

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export default function Login() {
     const client_id = process.env.REACT_APP_SPOTIFY_CLIENT_ID
@@ -6,9 +6,31 @@ export default function Login() {
     const auth_endpoint = 'https://accounts.spotify.com/authorize'
     const response_type = 'token'
 
+    const [token, setToken] = useState("")
+
+    useEffect(() => {
+        const hash = window.location.hash
+        let token = window.localStorage.getItem("token")
+
+        if (!token && hash) {
+            token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+
+            window.location.hash = ""
+            window.localStorage.setItem("token", token)
+            setToken(token)
+        }
+    }, [])
+
+    const logout = () => {
+        setToken("")
+        window.localStorage.removeItem("token")
+    }
+
     return (
-        <div className='login'>
-            <a href={`${auth_endpoint}?response_type=${response_type}&client_id=${encodeURIComponent(client_id)}&redirect_uri=${encodeURIComponent(redirect_uri)}`}>Login</a>
+        <div className='loginOut'>
+            {!token
+                ? <a href={`${auth_endpoint}?response_type=${response_type}&client_id=${encodeURIComponent(client_id)}&redirect_uri=${encodeURIComponent(redirect_uri)}`}>Login</a>
+                : <a href="/" onClick={logout}>Logout</a>}
         </div>
     )
 }
